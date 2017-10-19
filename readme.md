f206176db489aedb013d73b706b19df6bd7655c8
tonetheman <tonetheman@gmail.com>
2017-10-18 21:22:38 -0500

    winner done

--- readme.md
+++ readme.md
@@ -1,3 +1,28 @@
+
+Read a code golf thing that was trying to output a starry night image.
+
+Winner here: seed 0, blur 32
+D:\projects\starry_night_1k>python sn_checked.py
+('running seed', 0)
+('running seed', 2)
+('running seed', 4)
+('running seed', 6)
+('running seed', 8)
+('running seed', 10)
+('running seed', 12)
+('running seed', 14)
+('running seed', 16)
+('running seed', 18)
+('running seed', 20)
+lowest 11177.9151557
+seed 21
+winner_seed 0
+winner blur 32
+total number of images examined 1890
+running winner now...
+
+
+
 see here
 
 https://codegolf.stackexchange.com/questions/69930/paint-starry-night-objectively-in-1kb-of-code/70907#70907
--- sn_checked.py
+++ sn_checked.py
@@ -1,8 +1,8 @@
 
 
 
-def doitall(_seed):
-    from PIL import Image, ImageDraw
+def doitall(_seed, _blur):
+    from PIL import Image, ImageDraw, ImageFilter
     import random
 
     # NOTE THIS IS NOT golfed not trying to make small yet
@@ -39,6 +39,10 @@ def doitall(_seed):
         draw.ellipse((x,y,x+diam,y+diam), fill=color,
             outline=color)
 
+    #for i in range(2):
+    #    output_image = output_image.filter(ImageFilter.BLUR)
+    # output_image = output_image.filter(ImageFilter.GaussianBlur(10))
+    output_image = output_image.filter(ImageFilter.BoxBlur(_blur))
     output_image.save("output.png")
 
 
@@ -49,30 +53,45 @@ def checkit():
     return float(res)
 
 def runcontest():
+    total_count = 0
     lowest = 1000000
     seed = 0
     winner_seed = -1
+    winner_blur = -1
     while True:
-        if seed%10 == 0:
+        # print something out every 10 picks
+        if seed%2 == 0:
             print("running seed",seed)
-        doitall(seed)
-        res = checkit()
 
-        if res < lowest:
-            lowest = res
-            winner_seed = seed
+        # check 50 blur values
+        for j in range(90):
 
+            # run the check
+            doitall(seed,j)
+            res = checkit()
+            total_count = total_count + 1
+
+            # remember the lowest
+            if res < lowest:
+                lowest = res
+                boxblur = j
+                winner_seed = seed
+                winner_blur = j
+
+        # bump the seed
         seed = seed + 1
 
-        if seed>100:
+        # stop at 10 seeds
+        if seed>20:
             break
 
     print "lowest",lowest
     print "seed",seed
     print "winner_seed",winner_seed
-
+    print "winner blur", winner_blur
+    print "total number of images examined", total_count
     print("running winner now...")
-    doitall(winner_seed)
+    doitall(winner_seed,winner_blur)
 
 
 
